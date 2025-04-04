30ea44e27d314eb207446551642bc0de3d7cceea
coti <162303357+coticelli@users.noreply.github.com>
2025-04-04 07:57:24 +0000

    Implementa la strategia di autenticazione Google con verifica dell'email e creazione utente nel database

--- server.js
+++ server.js
@@ -93,27 +93,57 @@ const db = new sqlite3.Database('./database.db');
 
 
 // Configure Google Strategy
+// Configure Google Strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL,
     scope: ['profile', 'email']
 },
-function(accessToken, refreshToken, profile, done) {
-    // Qui normalmente cerchi o crei l'utente nel database
-    // Esempio:
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return done(err, user);
+(accessToken, refreshToken, profile, done) => {
+    // Check if user exists in database using email
+    const email = profile.emails && profile.emails.length > 0
+        ? profile.emails[0].value
+        : null;
+   
+    if (!email) {
+        return done(new Error('No email found in Google profile'), null);
+    }
+   
+    db.get('SELECT * FROM utenti WHERE email = ?', [email], (err, user) => {
+        if (err) {
+            return done(err);
+        }
+       
+        if (user) {
+            // User exists, return user
+            return done(null, user);
+        } else {
+            // User doesn't exist, create new user
+            const newUser = {
+                username: profile.displayName || email.split('@')[0],
+                email: email,
+                password: bcrypt.hashSync(Math.random().toString(36).slice(-8), 10), // Random password
+                tipo: 'cliente' // Default user type
+            };
+           
+            db.run('INSERT INTO utenti (username, email, password, tipo) VALUES (?, ?, ?, ?)',
+                [newUser.username, newUser.email, newUser.password, newUser.tipo],
+                function(err) {
+                    if (err) {
+                        return done(err);
+                    }
+                   
+                    // Get the inserted user
+                    newUser.id = this.lastID;
+                    return done(null, newUser);
+                }
+            );
+        }
     });
-    
-    // Oppure per test:
-    // return done(null, {
-    //     id: profile.id,
-    //     username: profile.displayName,
-    //     email: profile.emails[0].value,
-    //     tipo: 'cliente' // Default tipo
-    // });
 }));
+
+
 (accessToken, refreshToken, profile, done) => {
     // Check if user exists in database using email
     const email = profile.emails && profile.emails.length > 0
